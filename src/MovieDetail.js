93485aa2e019f162b710156a490219cadb4da09b
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 12:25:04 -0500

    commit

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import * as MovieAPI from './MovieAPI';
 
 class MovieDetail extends React.Component {
   constructor() {
@@ -7,6 +7,49 @@ class MovieDetail extends React.Component {
     this.state = {
       movie: {}
     }
+  }
+
+  componentDidMount() {
+    MovieAPI.getAll()
+      .then((movies) => {
+        let movie = movies.find(m => m.id === this.props.match.params.id)
+        console.log(movie)
+        this.setState({movie: movie})
+      })
+  }
+
+  addMylist = (add) => {
+    if(add) {
+      MovieAPI.removeFromList(this.state.movie);
+    } else {
+      MovieAPI.addToList(this.state.movie);
+    }
+  }
+
+  render() {
+    console.log(this.state.movie)
+    return (
+      <div className="movie">
+        {/*<a href={"/detail/"+this.state.movie.id}>
+          <img
+            src={this.state.movie.poster_path}
+          />
+          <div className="overlay">
+            <div className="title">Ant-Man</div>
+            <div className="rating">7.1/10</div>
+            <div className="plot">
+              {this.state.movie.overview}
+            </div>
+          </div>
+        </a>
+        <div data-toggled={this.state.movie.my_list} onClick={() => this.addMylist(this.state.movie.my_list)} className="listToggle">
+          <div>
+            <i className="fa fa-fw fa-plus"></i
+            ><i className="fa fa-fw fa-check"></i>
+          </div>
+        </div>*/}
+      </div>
+    );
   }
 }
 
