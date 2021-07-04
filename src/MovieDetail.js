b64f6811476fa2b9f0748a542a5b45182ff51b74
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 12:06:42 -0500

    commit

---
+++ src/MovieDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class MovieDetail extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {}
+    }
+  }
+}
+
+export default MovieDetail;
