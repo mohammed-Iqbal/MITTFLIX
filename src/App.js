3c32d9c2802f365faf92ae73dbd32d2efe123f35
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 11:42:26 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
+import MovieList from './MovieList';
+
 import * as MovieAPI from './MovieAPI';
 
 class App extends React.Component {
   render = () => {
     return (
       <>
-       
+        <Route
+          exact
+          path="/"
+          component={MovieList}
+        />
       </>
     );
   }
