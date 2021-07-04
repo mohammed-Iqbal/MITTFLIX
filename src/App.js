0d8adc2b82a2780b3cd31f69f55625d2a72f713f
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 16:59:31 -0500

    Removing deatils page as per instruction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
 import MovieList from './MovieList';
-import MovieDetail from './MovieDetail';
 import Mylist from './Mylist';
 import SearchList from './SearchList';
-
 import * as MovieAPI from './MovieAPI';
 
 class App extends React.Component {
@@ -21,12 +19,6 @@ class App extends React.Component {
           exact
           path="/myList"
           component={Mylist}
-        />
-
-        <Route
-          exact
-          path="/detail/:id"
-          component={MovieDetail}
         />
 
         <Route
