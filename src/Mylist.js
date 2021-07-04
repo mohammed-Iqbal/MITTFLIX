b9de01df109bbe2d2d652313d985e1e6843be794
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 13:04:36 -0500

    comit

---
+++ src/Mylist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class MovieList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+}
+export default MovieList;
