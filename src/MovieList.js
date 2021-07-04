1e76767b2efa5ce654c1ece94c256a0b1daad729
Mohammed Iqbal <79719388+mohammed-Iqbal@users.noreply.github.com>
2021-07-04 11:14:57 -0500

    commit

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import * as MovieAPI from './MovieAPI';
+import Header from './header';
+import MovieItem from './MovieItem';
 
 class MovieList extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        genres: [],
-        movies: []
-      }
+  constructor() {
+    super();
+    this.state = {
+      genres: [],
+      movies: []
     }
-  
-    componentDidMount() {
-      this.getData();
-    }
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    MovieAPI.getAll()
+      .then((movies) => {
+        this.setState({movies: movies});
+        MovieAPI.genres()
+        .then((genres) => {
+          let sort = [];
+          for (let i = genres.length - 1; i >= 0; i--) {
+            let len = movies.filter(m => m.genre_ids.indexOf(genres[i].id)>0).length;
+            if(len > 0){
+              sort.push(genres[i])
+            }
+          }
+          sort.sort((s1, s2) => {
+            if(s1.name.toLowerCase() < s2.name.toLowerCase()) {
+              return -1;
+            } else {
+              return 1;
+            }
+          });
+          this.setState({genres: sort});
+        })
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+
+        {this.state.genres.map((genre, i) => (
+          <div className="titleList" key={i}>
+            <div className="title">
+              <h1>{genre.name}</h1>
+              <div className="titles-wrapper">
+                {this.state.movies.filter(m => m.genre_ids.indexOf(genre.id)>0).map((movie) => (
+                  <MovieItem key={movie.id} movie = {movie} getData={this.getData}/>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
+
+export default MovieList;
